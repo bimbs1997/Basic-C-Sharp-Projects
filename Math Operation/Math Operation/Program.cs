e13ef6a27a42473f44563f10ce5b954a3671c455
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-02-11 15:16:39 -0500

    Re build

--- Math Operation/Math Operation/Program.cs
+++ Math Operation/Math Operation/Program.cs
@@ -10,27 +10,36 @@ namespace Math_Operation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter your Card number.");
-            string cardnumber = Console.ReadLine();
-            int consumers = 42 * 50;
-            Console.WriteLine("Your slot number is: " + consumers);
+            Console.WriteLine("Please enter your favorite number.");
+            int Cardnumber = Convert.ToInt32(Console.ReadLine());
+            Cardnumber = Cardnumber * 50;
+            Console.WriteLine(Cardnumber);
             Console.ReadLine();
 
-            //int vendor = 12 + 25;
-            //Console.WriteLine(vendor);
-            //Console.ReadLine();
+            Console.WriteLine("Add up your result on 1st step.");
+            int Secondnumber = Convert.ToInt32(Console.ReadLine());
+            Secondnumber = Secondnumber + 25;
+            Console.WriteLine(Secondnumber);
+            Console.ReadLine();
 
-            //double speed = 92.0 / 12.5;
-            //Console.WriteLine(speed);
-            //Console.ReadLine();
+            Console.WriteLine("Divide your result on 2nd step");
+            double Thirdnumber = Convert.ToInt32(Console.ReadLine());
+            Thirdnumber = Thirdnumber / 12.5;
+            Console.WriteLine(Thirdnumber);
+            Console.ReadLine();
 
-            //bool TrueOrFalse = 66 > 50;
-            //Console.WriteLine(TrueOrFalse.ToString());
-            //Console.ReadLine();
+            Console.WriteLine("3rd step Lets see if its TRUE or FALSE");
+            int Fourthnumber = Convert.ToInt32(Console.ReadLine());
+            bool TrueOrFalse = Fourthnumber > 50;
+            Console.WriteLine(TrueOrFalse.ToString());
+            Console.ReadLine();
 
-            //int remainder = 1021 % 7;
-            //Console.WriteLine(remainder);
-            //Console.ReadLine();
+            Console.WriteLine("Please enter the last 2 digit on the 3rd step");
+            int remainder = Convert.ToInt32(Console.ReadLine());
+            remainder = remainder % 7;
+            Console.WriteLine(remainder);
+            Console.WriteLine("That's Your Lucky Number Hooray!!");
+            Console.ReadLine();
 
 
 
