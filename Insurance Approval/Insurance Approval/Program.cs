58169138d5268185c35532db141474c155de81b0
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-02-18 16:55:43 -0500

    Insurance Approval Program

---
+++ Insurance Approval/Insurance Approval/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insurance_Approval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+            Console.WriteLine("Have you ever had a DUI? True Or False");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int ticket = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+            Console.WriteLine("Qualified?");
+            bool QualifiedOrNot = age > 15 ||  DUI  ||  ticket < 3;
+            Console.WriteLine(QualifiedOrNot);
+            Console.ReadLine();
+
+        }
+    }
+}
