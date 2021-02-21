6aea369dda3f6d220ee7bc3434fff36d5fe5dcfc
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-02-21 15:40:08 -0500

    Price-Quote Application Assignment

---
+++ Price-Quote Application/Price-Quote Application/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Price_Quote_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter the package weight:");
+            int pweight = Convert.ToInt32(Console.ReadLine());
+
+
+            if (pweight < 50)
+            {
+                Console.WriteLine("The Package is weight is perfect!");
+            }
+            else
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine(); 
+                Environment.Exit(0);
+            }
+            Console.WriteLine("Please enter the package width:");
+            int pwidth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height:");
+            int pheight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length:");
+            int plength = Convert.ToInt32(Console.ReadLine());
+
+            if (pwidth < 50 && pheight < 50 && plength < 50)
+            {
+                
+                decimal quote = Convert.ToDecimal(pwidth * pheight * plength * pweight / 100);
+                Console.WriteLine("Your estimated total for shipping this package is:$" + quote);
+                Console.WriteLine("Thank you for your patience. Have A Great Day!");
+                Console.ReadLine();         
+            }
+            else
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
