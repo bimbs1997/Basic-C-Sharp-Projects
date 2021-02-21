8da538b47a7a1051094c04fc673474ac3c179367
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-02-21 18:00:00 -0500

    Loop Assignment Submission

---
+++ Loop Assignment/Loop Assignment/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Type a number and i'll show you some magic.");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //bool showmagic = number == 11;
+
+            //while (!showmagic)
+            //{
+            //    switch (number)
+            //    {
+            //        case 24:
+            //            Console.WriteLine("You choose 24, The good energy is within you and you'll always be lucky on love. ");
+            //            Console.WriteLine("Type a number and i'll show you some magic.");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 14:
+            //            Console.WriteLine("You choose 14, You will always provide good vibes and loved by everyone.");
+            //            Console.WriteLine("Type a number and i'll show you some magic.");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 69:
+            //            Console.WriteLine("You choose 69, The pleasure is always within you but lack of love.");
+            //            Console.WriteLine("Type a number and i'll show you some magic.");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 11:
+            //            Console.WriteLine("You choose 11, Hooray!! You guessed my favorite number. Good JOB!");
+            //            showmagic = true;
+            //            break;
+            //        case 17:
+            //            Console.WriteLine("You choose 17, You always gonna have some good memories this year, few are bad but big. ");
+            //            Console.WriteLine("Type a number and i'll show you some magic.");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        default:
+            //            Console.WriteLine("Pick a number again");
+            //            Console.WriteLine("Type a number and i'll show you some magic.");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //Console.WriteLine("Pick your Box by typing your number.");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //bool box = number == 11;
+
+            //do
+            //{
+            //    switch (number)
+            //    {
+            //        case 40:
+            //            Console.WriteLine("You choose 40, Hooraay!!  You Got Brand new Apple MacBook. ");
+            //            Console.WriteLine("Type a number again and lets see your luck.");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 16:
+            //            Console.WriteLine("You choose 16, Hooraay!! You open the box filled with designer clothes.");
+            //            Console.WriteLine("Type a number again and lets see your luck.");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 10:
+            //            Console.WriteLine("You choose 10, Hooraay!! You won a brand QLED LG TV.");
+            //            Console.WriteLine("Type a number again and lets see your luck.");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 11:
+            //            Console.WriteLine("You choose 11, Hooraay!! You guessed the JACKPOT PRIZE Nissan 370z . Good JOB!");
+            //            box = true;
+            //            break;
+            //        case 20:
+            //            Console.WriteLine("You choose 20, Hooraay!! You have won a brand new xbox one series X. ");
+            //            Console.WriteLine("Type a number again and lets see your luck.");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        default:
+            //            Console.WriteLine("Pick a number again");
+            //            Console.WriteLine("Type a number again and lets see your luck.");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //    }
+            //}
+            //while (!box);
+            //Console.ReadLine();
+
+        }
+    }
+}
