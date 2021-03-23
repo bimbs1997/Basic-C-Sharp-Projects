9cf2373a5bdc8f59d0e1b33daa5867ffeea76b1b
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-03-23 15:36:28 -0400

    Classes and Methods Part 2

---
+++ Classes and methods assignment 2/Classes and methods assignment 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_methods_assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            methods method = new methods();
+
+            int first, second;
+            string choice;
+            Console.Write("Enter first number: ");
+            first = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nSecond number is optional! Do you want to enter? Press 'y' or 'Y' for it : ");
+            choice = Console.ReadLine();
+            if(choice == "y" || choice == "Y")
+            {
+                Console.Write("\nEnter Second number: ");
+                second = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nAnswer: " + method.sum(first, second));
+            }
+            else
+            {
+                Console.WriteLine("\nAnswer: " + method.sum(first));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Classes and methods assignment 2/Classes and methods assignment 2/methods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_methods_assignment_2
+{
+    class methods
+    {
+        public int sum(int first, int second = 10)
+        {
+            return first + second;
+        }
+    }
+}
