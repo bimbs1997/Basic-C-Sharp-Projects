6c526c11f1e77739924376ccaf84d71ced31da0e
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-03-23 13:10:58 -0400

    Method Program

---
+++ Method Assignment/Method Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            methods method = new methods();
+            Console.Write("Enter Number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Square : " + method.square(num));
+            Console.WriteLine("Cube : " + method.cube(num));
+            Console.WriteLine("Half : " + method.half(num));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Method Assignment/Method Assignment/methods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    class methods
+    {
+        public int square(int x)
+        {
+            return x * x;
+        }
+
+        public int cube(int x)
+        {
+            return  x * x * x;
+        }
+
+        public int half(int x)
+        {
+            return x/2;
+        }
+    }
+}
