83fc553ff8af5fcbf637a70ab2da1492919f23b5
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-03-23 14:40:29 -0400

    Classes and Method Part 1

---
+++ Classes And Method Assignment 1/Classes And Method Assignment 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_And_Method_Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            decimal number;
+            string val;
+            Methods method = new Methods();
+            
+            //Integer Parameter
+            Console.Write("Enter number for integer parameter method: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Return value of integer parameter method: " + method.Square(num));
+
+            //Decimal parameter
+            Console.Write("\n\nEnter number for Decimal parameter method: ");
+            number = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Return value of Decimal parameter method: " + method.Square(number));
+
+            //String parameter
+            Console.Write("\n\nEnter number for String parameter method: ");
+            val = Console.ReadLine();
+            Console.Write("Return value of integer parameter method: " + method.Square(val));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Classes And Method Assignment 1/Classes And Method Assignment 1/methods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_And_Method_Assignment_1
+{
+    class Methods
+    {
+        public int Square(int x)
+        {
+            return x + 10;
+        }
+
+        public int Square(decimal x)
+        {
+            return Convert.ToInt32(x + 10);
+        }
+        public int Square(string x)
+        {
+            return Convert.ToInt32(x) + 10;
+        }
+    }
+}
