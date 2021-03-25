54d4c5e47b95f25ce0c19b064213fe687efbb293
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-03-25 16:29:50 -0400

    Classes and Method Part 3

---
+++ Classes and Methods Assignment 3/Classes and Methods Assignment 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_Assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            methods obj = new methods();
+            //Passing numbers as parameter
+            Console.Write("Output when we pass numbers as Parameter: ");
+            obj.test(10, 78);
+
+            int first = 10;
+            int second = 78;
+
+            //Passing name as parameter
+            Console.Write("Output when we pass Name as Parameter: ");
+            obj.test(first, second);
+
+
+            Console.ReadKey();
+        }
+    }
+}
