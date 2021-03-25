d82064e55763d391e601c3523ff181402cb8d975
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-03-25 13:58:07 -0400

    Classes and Method part 3

---
+++ Class method assignments/Class method assignments/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_method_assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number for divided it by 2: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            methods.divide(num);
+
+            int x = 20,y = 50;
+            Console.WriteLine("Value of x before Out Parameter mehtod: "+x+"\nValue of y before Out Parameter method: "+ y);
+            methods.sumOut(out x, out y);
+
+            Console.WriteLine("\nValue of x after Out Parameter mehtod: " + x + "\nValue of y after Out Parameter method: " + y);
+
+            //Overloaded
+            methods.sumOut(out x);
+            Console.WriteLine("\nValue of x after overlading Out Parameter mehtod: " + x );
+
+            Console.ReadKey();
+        }
+    }
+}
--- Class method assignments/Class method assignments/Properties/AssemblyInfo.cs
+++ Class method assignments/Class method assignments/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Classes and Methods Assignment 3")]
+[assembly: AssemblyTitle("Class method assignments")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Classes and Methods Assignment 3")]
+[assembly: AssemblyProduct("Class method assignments")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3b1c2e47-5bdb-4a38-8daf-15d434818987")]
+[assembly: Guid("3f58a047-2b07-4cce-8832-b3e2c93c9a7a")]
 
 // Version information for an assembly consists of the following four values:
 //
