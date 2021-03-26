cfdf662a0fecef955e2ca952daa14aaa31b597b0
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-03-26 14:53:46 -0400

    Overloading Program

---
+++ Overloading Assignment/Overloading Assignment/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Assignment
+{
+    class Employee : Person
+    {
+        public int Id {get; set;}
+
+        public void SayName()
+        {
+            base.SayName();
+        }
+
+        //Overloading the == operator for compairing the Employee ID
+        public static bool operator == (Employee emp1, Employee emp2)
+        {
+            return emp1.Id == emp2.Id;
+        }
+
+        //We have to ovveride the != operator when we try to override == operator
+        public static bool operator != (Employee emp1, Employee emp2)
+        {
+            return emp1.Id != emp2.Id;
+        }
+    }
+}
---
+++ Overloading Assignment/Overloading Assignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            Employee emp2 = new Employee();
+
+            emp1.Id = 10;
+            emp2.Id = 10;
+
+            Console.WriteLine("When Both Id's are same: ");
+            if(emp1 == emp2)
+            {
+                Console.WriteLine("Both id are same!");
+            }
+            else
+            {
+                Console.WriteLine("Id's are different!");
+            }
+
+            emp1.Id = 10;
+            emp2.Id = 20;
+            Console.WriteLine("\nWhen Both Id's are Different: ");
+            if (emp1 == emp2)
+            {
+                Console.WriteLine("Both id are same!");
+            }
+            else
+            {
+                Console.WriteLine("Id's are different!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
