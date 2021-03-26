667d5af7c2a416fb158cab160de9378a08daf6ff
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-03-26 18:44:04 -0400

    Generic Program

---
+++ Generic Assignment/Generic Assignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Assignment
+{
+    class Employee<T>
+    {
+        public List<T> Things {get; set;}
+        public Employee()
+        {
+            Things = new List<T>();
+        }
+    }
+}
---
+++ Generic Assignment/Generic Assignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> emp = new Employee<string>();
+            
+            emp.Things.Add("abc");
+            emp.Things.Add("def");
+            emp.Things.Add("ghi");
+            emp.Things.Add("jkl");
+
+            Console.WriteLine("When Data type is String: ");
+            for(int i = 0; i < emp.Things.Count; i++)
+            {
+                Console.WriteLine(emp.Things[i]);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("When Data type is INT: ");
+            Employee<int> empInt = new Employee<int>();
+            empInt.Things.Add(1);
+            empInt.Things.Add(2);
+            empInt.Things.Add(3);
+            empInt.Things.Add(4);
+
+            for (int i = 0; i < empInt.Things.Count; i++)
+            {
+                Console.WriteLine(empInt.Things[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
