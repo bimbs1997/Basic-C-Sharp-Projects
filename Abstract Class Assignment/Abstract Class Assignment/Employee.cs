e26e810070364a370aba125e3bf69d5e0c3dcc3b
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-03-26 00:45:07 -0400

    Abstract Program

---
+++ Abstract Class Assignment/Abstract Class Assignment/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    class Employee : Person, IQuittable
+    {
+        public int Id {get; set;}
+
+        public override void sayName()
+        {
+            Console.WriteLine("Name: " + base.FirstName + " " + base.LastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("I Quit!");
+        }
+
+    }
+}
