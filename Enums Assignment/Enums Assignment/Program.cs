30e0c6d1e9b7d435e43ca43a74526edcbeaaf59e
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-03-26 20:20:22 -0400

    Enums Program

---
+++ Enums Assignment/Enums Assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_Assignment
+{
+    class Program
+    {
+        enum DayOfWeek { sunday, monday, tuesday, wednesday, thursday, friday, saturday }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Ernter the current day of the week: ");
+                string currentday = Console.ReadLine();
+                DayOfWeek day;
+
+                if (Enum.TryParse<DayOfWeek>(currentday, out day))
+                {
+                    Console.WriteLine("Current Day is: " + currentday);
+                }
+                else
+                {
+                    throw new OverflowException();
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Please enter a actual day of week!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
