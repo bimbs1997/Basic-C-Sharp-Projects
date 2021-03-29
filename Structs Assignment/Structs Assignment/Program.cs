f47b06975cda449102790138401bf80d340ee940
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-03-28 23:52:10 -0400

    Structs assignment
    Made some changes on the program

---
+++ Structs Assignment/Structs Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs_Assignment
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers num = new Numbers();
+            num.Amount = 10;
+
+            Console.WriteLine("Amount is: " + num.Amount);
+            Console.ReadKey();
+        }
+    }
+}
