ea59f57297fa2b46d36e16cd944c55a017e8010e
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-03-30 08:59:19 -0400

    File I/O Assignment

---
+++ IO Assignment/IO Assignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"D:\IOAssignment.txt";
+
+            try
+            {
+                TextWriter tw = new StreamWriter(fileName);
+                Console.Write("Enter number: ");
+                tw.Write(Convert.ToInt32(Console.ReadLine()));
+                tw.Close();
+
+                Console.Write("\nText into the text file is : ");
+                using (StreamReader sr = File.OpenText(fileName))
+                {   
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
