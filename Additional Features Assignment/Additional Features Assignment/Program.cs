d475d8c64fd0f0cce9d44ce3ba79c030031e9f58
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-03-30 05:29:21 -0400

    Additional Features Program

---
+++ Additional Features Assignment/Additional Features Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            features fea = new features();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Additional Features Assignment/Additional Features Assignment/features.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features_Assignment
+{
+    class features
+    {
+        const int constVariable = 10;
+        public features() : this(constVariable)
+        {
+
+        }
+
+        public features(int variable)
+        {
+            var temp = constVariable;
+            variable = temp;
+            Console.WriteLine("Const variable value in chaining constructor: " + variable);
+        }
+    }
+}
