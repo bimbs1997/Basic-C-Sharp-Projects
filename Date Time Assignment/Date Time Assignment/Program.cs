52e34626545e51f43d73ce3189e11f4b45cae666
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2021-03-30 13:58:39 -0400

    Date Time Program

---
+++ Date Time Assignment/Date Time Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Time_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime.ToString());
+
+            Console.Write("How many hours you want to add: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+
+            DateTime newDate = dateTime.AddHours(num);
+            Console.WriteLine("Exact time after " + num + " hours will be: " + newDate.ToShortTimeString());
+            Console.ReadKey();
+        }
+    }
+}
